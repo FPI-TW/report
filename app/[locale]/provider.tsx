dd5fe977613263a228230866787ed32f150bc51d
Tyler <ty.ho1126@gmail.com>
2025-11-06 14:08:14 +0800

    feat: add timezone support and S3 presigned URL capability
    - Integrated timezone handling in next-intl provider by fetching and passing timezone from server to client
    - Refactored layout to use Promise.all for parallel data fetching and removed params prop in favor of direct locale retrieval
    - Added @aws-sdk/s3-request-presigner dependency for generating presigned S3 URLs

--- app/[locale]/provider.tsx
+++ app/[locale]/provider.tsx
@@ -18,14 +18,24 @@ type Props = {
   children: ReactNode
   messages: AbstractIntlMessages
   locale: string
+  timeZone: string
 }
 
-export default function Provider({ children, messages, locale }: Props) {
+export default function Provider({
+  children,
+  messages,
+  locale,
+  timeZone,
+}: Props) {
   const [queryClient] = useState(() => new QueryClient(queryConfig))
 
   return (
     <QueryClientProvider client={queryClient}>
-      <NextIntlClientProvider messages={messages} locale={locale}>
+      <NextIntlClientProvider
+        messages={messages}
+        locale={locale}
+        timeZone={timeZone}
+      >
         {children}
       </NextIntlClientProvider>
     </QueryClientProvider>
