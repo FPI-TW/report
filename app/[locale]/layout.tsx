21cd5cf6c1edc10a224c102f63ce38d67f866575
Tyler <ty.ho1126@gmail.com>
2025-12-29 11:02:49 +0800

    feat: update GA serial number.

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -54,7 +54,7 @@ export default async function RootLayout({
         {process.env.NODE_ENV !== "development" && (
           <>
             <Analytics />
-            <GoogleAnalytics gaId="G-3WBP4X8RFS" />
+            <GoogleAnalytics gaId="G-44DL50PYWW" />
           </>
         )}
       </body>
